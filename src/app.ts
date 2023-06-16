4a6c2fe465a6e97c274349efb58e1fbf56bcfef0
LBruner <leandrobruner15@gmail.com>
2023-06-15 21:16:51 -0300

    add logging tool

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import {startFilesSync} from "./models/files/files.model";
 import process from "process";
-
+import winston from 'winston';
 
 const app = express();
-
 
 app.use(cors({
     origin: 'http://localhost:8080',
@@ -15,10 +14,25 @@ app.use(cors({
 app.use(morgan('combined'))
 app.use(express.json());
 
-const documentsDirectory = '/home/lbruner/Documents/Cursos';
+export const itemsLogger = winston.createLogger({
+    level: 'info', // Set the log level
+    format: winston.format.simple(), // Define the log format
+    transports: [
+        new winston.transports.Console(), // Log to the console
+        new winston.transports.File({filename: 'items.log'}), // Log to a file
+    ],
+});
 
-startFilesSync(documentsDirectory, process.env.DOCUMENTS_FOLDER_KEY!)
-// startFilesSync(screenshotsDirectory, process.env.SCREENSHOTS_FOLDER_KEY!)
+export const driveLogger = winston.createLogger({
+    level: 'info', // Set the log level
+    format: winston.format.simple(), // Define the log format
+    transports: [
+        new winston.transports.Console(), // Log to the console
+        new winston.transports.File({filename: 'drive.log'}), // Log to a file
+    ],
+});
+
+startFilesSync(process.env.CURSOS_DIRECTORY!, process.env.DOCUMENTS_FOLDER_KEY!)
 
 app.get('/*', (req, res) => {
     res.send("OI");
