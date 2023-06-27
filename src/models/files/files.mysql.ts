8b83d3e7c827d3f095c7c55c56cf978409da1955
LBruner <leandrobruner15@gmail.com>
2023-06-26 22:16:29 -0300

    add unused items clean up

--- src/models/files/files.mysql.ts
+++ src/models/files/files.mysql.ts
@@ -1,7 +1,6 @@
 import query from "../../services/mysql";
 import {TableIdentifier} from "../../services/types";
 
-
 export async function getRegisteredItem(path: string, tableIdentifier: TableIdentifier): Promise<{
     cloudID: string | undefined
 }> {
@@ -11,26 +10,3 @@ export async function getRegisteredItem(path: string, tableIdentifier: TableIden
 
     return results;
 }
-
-//
-// export const registerItem = async (item: Folder | File, parentFolderID: string): Promise<string> => {
-//     const cloudID = await uploadItem(item, parentFolderID);
-//     if ('size' in item) {
-//         const {name, extension, path, size, modifiedDate, parentFolder} = item;
-//         itemsLogger.info(`\`FILE: ${item.name} was uploaded to parent: ${parentFolderID} and Has ID: ${cloudID}`)
-//         await query(`INSERT INTO files (name, extension, path, parentFolder, cloudID, lastModifiedLocal,
-//                                         lastModifiedCloud,
-//                                         size) VALUE ("${name}", "${extension}", "${path}", "${parentFolder}",
-//                                                      "${cloudID}", "${modifiedDate}",
-//                                                      "${modifiedDate}", "${size}")`)
-//     } else {
-//         const {name, path, modifiedDate} = item;
-//         itemsLogger.info(`\`FOLDER: ${item.name} was uploaded to parent: ${parentFolderID} and Has ID: ${cloudID}`)
-//         await query(`INSERT INTO folders (name, path, cloudID, modifiedDate)
-//                      VALUES ("${name}", "${path}", "${cloudID}", "${modifiedDate}")`);
-//     }
-//
-//     return parentFolderID;
-// }
-
-
