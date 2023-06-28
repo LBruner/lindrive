0ddfb9b43cced1d2815c8ba373a709293ffe090d
LBruner <leandrobruner15@gmail.com>
2023-06-28 15:33:47 -0300

    finish chokidar implementation

--- src/models/files/files.mysql.ts
+++ src/models/files/files.mysql.ts
@@ -1,9 +1,13 @@
 import query from "../../services/mysql";
 import {TableIdentifier} from "../../services/types";
 
-export async function getRegisteredItem(path: string, tableIdentifier: TableIdentifier): Promise<{
+export const getRegisteredItem = async (path: string, tableIdentifier: TableIdentifier): Promise<{
     cloudID: string | undefined
-}> {
+}> => {
+
+    console.log(`SELECT cloudID
+                 from ${tableIdentifier}
+                 WHERE path = "${path}"`)
     const [results] = await query(`SELECT cloudID
                                    from ${tableIdentifier}
                                    WHERE path = "${path}"`);
