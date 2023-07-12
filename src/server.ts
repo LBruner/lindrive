e8eff771378c0b29bf7048c98510bd47bb32161a
LBruner <leandrobruner15@gmail.com>
2023-07-12 17:15:41 -0300

    add local storage feature

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,7 @@ const PORT = 8080;
 const server = http.createServer(app);
 const startServer = async () => {
     await setupDatabase();
-    //TODO fix this
-    UserManager.getInstance()
+    UserManager.getInstance().addTrackingPath(process.env.CURSOS_DIRECTORY!);
     server.listen(PORT, () => {
         console.log(`Listening on port: ${PORT}...`);
     })
