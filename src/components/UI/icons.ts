c58e5f93975ddceba1f79f76bf1f8f5a34516db1
LBruner <leandrobruner15@gmail.com>
2023-08-19 15:09:09 -0300

    chore: add font awesome icons

---
+++ src/components/UI/icons.ts
@@ -0,0 +1,6 @@
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faGear, faMoon, faHardDrive, faSquarePlus, faHome, faRightFromBracket, faFolder, faX} from "@fortawesome/free-solid-svg-icons";
+
+export const setupIcons = () => {
+    library.add(faGear, faMoon, faHardDrive, faSquarePlus, faHome, faRightFromBracket, faFolder,faX);
+}
