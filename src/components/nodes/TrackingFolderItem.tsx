4df064df061cf5bc320468e6dfad3bc8fb1c9e0a
LBruner <leandrobruner15@gmail.com>
2023-09-24 18:57:59 -0300

    style: update tracking folder screen

--- src/components/nodes/TrackingFolderItem.tsx
+++ src/components/nodes/TrackingFolderItem.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import styled from 'styled-components';
 import {ClientEvents} from "../../../events";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 interface TrackingFolderProp {
     name: string,
@@ -9,28 +7,49 @@ interface TrackingFolderProp {
 }
 
 const TrackingFolderItem: React.FC<TrackingFolderProp> = ({name, path}) => {
-    const Container = styled.div`
-      padding: 20px;
-    `
     const onDeleteTrackingFolder = () => {
         window.Main.send(ClientEvents.deleteTrackingFolder, path);
     };
 
     return (
-        <Container>
-            <div>
-                <FontAwesomeIcon icon={'fa-folder'} size={"1x"}/>
+        <>
+            <div className="modal fade" id="exampleModal" tabIndex={1} aria-labelledby="deleteModal"
+                 aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="deleteModal">Warning</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">X
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="shadow-none">
+                                <p className={'text-danger fs-4'}>
+                                    All the files and folders will be deleted <span className={'fw-bolder'}>on your Google Drive folder</span>.
+
+                                </p>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={onDeleteTrackingFolder}>Delete</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-                {name}
+            <div className="list-group">
+                <div className="list-group-item list-group-item-action" aria-current="true">
+                    <div className="d-flex w-100 justify-content-between align-items-center">
+                        <h5 className="mb-1">{name}</h5>
+                        <button type="button" className="btn btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                            Delete
+                        </button>
+                    </div>
+                    <p className="mb-1">{path}</p>
+                </div>
             </div>
-            <div>
-                {path}
-            </div>
-            <button onClick={onDeleteTrackingFolder}>
-                Delete
-            </button>
-        </Container>
+        </>
     )
 }
 
--- src/components/nodes/TrackingFoldersSettings.tsx
+++ src/components/nodes/TrackingFoldersSettings.tsx
@@ -1,9 +1,6 @@
 import React, {useEffect, useState} from "react";
-import styled from "styled-components";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import TrackingFolderItem from "./TrackingFolderItem";
-import Modal from "../UI/Modal";
 import {ClientEvents, ServerEvents} from "../../../events";
+import TrackingFolderItem from "./TrackingFolderItem";
 
 interface Folder {
     name: string,
@@ -21,9 +18,8 @@ const TrackingFoldersSettings: React.FC = () => {
             setTrackingFolders(trackingFolders);
         });
 
-        window.Main.on(ServerEvents.sendDeletedTrackingFolder, (deletedPath: string) => {
-            const newTrackingFolders = trackingFolders.filter(folder => folder.path !== deletedPath);
-            setTrackingFolders(newTrackingFolders);
+        window.Main.on(ServerEvents.sendDeletedTrackingFolder, () => {
+            window.Main.send(ClientEvents.getTrackingFolders);
         });
 
         return (() => {
@@ -33,56 +29,12 @@ const TrackingFoldersSettings: React.FC = () => {
         })
     }, []);
 
-
-    const MainContainer = styled.div`
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      height: 100%;
-      width: 100%;
-    `
-
-    const Wrapper = styled.div`
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-template-rows: 10% 80%;
-      background-color: azure;
-      padding: 15px;
-      width: 40rem;
-      height: 30rem;
-      border-radius: 10px;
-    `
-
-    const UpperContainer = styled.div`
-      height: 100%;
-      display: grid;
-      grid-template-columns: 1fr 3%;
-      padding: 15px;
-    `
-
-    const BottomContainer = styled.div`
-      display: grid;
-      flex-direction: column;
-      padding: 10px;
-      gap: 5px;
-    `
-
     return (
-        <Modal>
-            <MainContainer>
-                <Wrapper>
-                    <UpperContainer>
-                        <p>Tracking Folders</p>
-                        <FontAwesomeIcon icon={'fa-x'} size={"1x"}/>
-                    </UpperContainer>
-                    <BottomContainer>
-                        {trackingFolders.map(folder => <TrackingFolderItem key={folder.name} name={folder.name}
-                                                                           path={folder.path}/>)}
-                    </BottomContainer>
-                </Wrapper>
-            </MainContainer>
-        </Modal>
+        <>
+            <h6 className={'text-dark alert alert-light'}>Here you can remove folders that you are tracking</h6>
+            {trackingFolders.map(folder => <TrackingFolderItem key={folder.name} name={folder.name}
+                                                               path={folder.path}/>)}
+        </>
     )
 }
 
